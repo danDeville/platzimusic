8c9f7dff22312543c816265cd9694a2db38b477b
danDeville <dandeville26@gmail.com>
2018-12-04 14:09:57 -0500

    Inicial commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png"/>
+    <h1>PlatziMusic</h1>
+
+    <select v-model="selectedCountry">
+      <option v-for="country in countries" v-bind:value="country.value">{{ country.name }}</option>
+    </select>
+
+    <spinner v-show="loading"></spinner>
+
+    <ul>
+      <artist v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid"></artist>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import Artist from './components/Artist.vue'
+import getArtists from './api'
+import spinner from './components/spinner.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        {name: 'Argentina', value: 'argentina'},
+        {name: 'Colombia', value: 'colombia'},
+        {name: 'España', value: 'spain'},
+      ],
+      selectedCountry: 'argentina',
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    spinner
+  },
+  methods: {
+    refreshArtist(){
+      const self = this
+      this.loading = true
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.loading = false
+          self.artists = artists
+        })
+    }
+  },
+  mounted() {
+    this.refreshArtist()
+  },
+  watch: {
+    selectedCountry: function() {
+      this.refreshArtist()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
+
+  h1, h2
+    font-weight normal
+
+  ul
+    list-style-type none
+    padding 0
+
+  li
+    display inline-block
+    margin 0 10px
+
+  a
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+export default {
+  appName: 'platzimusic',
+  apiKey: '68b9132daf5230f43aae735f29972d94',
+  secret:	'eb945581b627270eea168884b7ca3653',
+  registeredTo:	'danDeville26'
+}
+// Application name	PlatziMusic
+// API key	68b9132daf5230f43aae735f29972d94
+// Shared secret	eb945581b627270eea168884b7ca3653
+// Registered to	danDeville26
